e81c3b9cd35d9509ca7ec1bee262b52b041dbfc4
mcnold <michele.godleske@gmail.com>
2021-08-03 22:41:38 -0400

    Tamagotchi part 1

---
+++ JS/app.js
@@ -0,0 +1,45 @@
+//Tamagotchi Project
+
+class Tamagotchi {
+    constructor(hunger,sleepiness,boredom,age){
+    this.hunger=[1,2,3,4,5,6,7,8,9,10]
+    this.sleepiness=[1,2,3,4,5,6,7,8,9,10]
+    this.boredom=[1,2,3,4,5,6,7,8,9,10]
+    this.age=age
+}
+feedPet() {
+
+}
+lightSwitch() {
+
+}
+
+playWithPet() {
+
+}
+
+namePet() {
+
+}
+
+increaseAge() {
+
+}
+
+increaseHunger() {
+
+}
+
+increaseSleepiness() {
+
+}
+
+increaseBoredom() {
+
+}
+
+morphAge() {
+
+}
+
+}
