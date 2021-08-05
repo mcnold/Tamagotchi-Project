ccb58f72b8f00d3a5a12bb149aa492b36fc90e03
mcnold <michele.godleske@gmail.com>
2021-08-04 20:07:08 -0400

    First commit

--- JS/app.js
+++ JS/app.js
@@ -7,6 +7,9 @@ class Tamagotchi {
     this.boredom=[1,2,3,4,5,6,7,8,9,10]
     this.age=age
 }
+logSubmit() {
+    log.textContent = "Hi! I'm "
+}
 feedPet() {
 
 }
@@ -19,7 +22,7 @@ playWithPet() {
 }
 
 namePet() {
-
+    document.querySelector("#submit").addEventListener('click',(event) => {
 }
 
 increaseAge() {
