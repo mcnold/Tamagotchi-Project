5a0a173e3dd138263db561730031dea8e5b6e1c5
mcnold <michele.godleske@gmail.com>
2021-08-06 22:02:15 -0400

    final draft commit

--- JS/app.js
+++ JS/app.js
@@ -15,12 +15,15 @@ class Tamagotchi {
     this.hunger=0
     this.sleepiness=0
     this.boredom=0
+    this.width=300
+    this.height=300
     }
-
-    
+   
     feedPet() {
+        console.log(this.hunger)
         this.hunger -=1
         document.querySelector("#hunger").innerText=`${this.hunger}`
+        console.log(this.hunger)
     }
    
   //  lightSwitchOff() {
@@ -34,99 +37,104 @@ class Tamagotchi {
     putToBed() {
         this.sleepiness -=1
         document.querySelector('#sleepiness').innerText=`${this.sleepiness}`
+        const kittyBed = document.querySelector('#kitty')
+        kittyBed.setAttribute('src', 'Images/Annoyed.png')
     }
 
     playWithPet() {
         this.boredom -=1
         document.querySelector('#boredom').innerText=`${this.boredom}`
+        const kittyPlay = document.querySelector('#kitty')
+        kittyPlay.setAttribute('src', 'Images/Happy.png')
 
     }
     giveLove() {
-        this.boredom -=1
-        this.sleepiness -=1
-        document.querySelector('#boredom').innerText=`${this.boredom}`
-        document.querySelector('#sleepiness').innerText=`${this.boredom}`
+        const kittyLoved = document.querySelector('#kitty')
+        kittyLoved.setAttribute('src', 'Images/Loved.png')
     }
 
     increaseAge() {
-        const ageID = setInterval(()=>{
+        this.ageID = setInterval(()=>{
             const ageElement =  document.querySelector('#age')
             ageElement.innerText= `${this.age}`
-            this.age ++
+            this.age++
                 if (this.age>3) {
-                    clearInterval(ageID)
+                    this.endGame()
                 }
-                this.morphAge()
         }, 15000)
     }
 
     increaseHunger() {
-        const hungerID = setInterval(()=>{
+        this.hungerID = setInterval(()=>{
             const hungerElement =  document.querySelector('#hunger')
             hungerElement.innerText=`${this.hunger}`
-            this.hunger ++
+            this.hunger++
                 if (this.hunger>10) {
-                    clearInterval(hungerID)
+                    this.endGame()
                 }
-        }, 1000)
+        }, 1500)
     }
 
     increaseSleepiness() {
-        const sleepyID = setInterval(()=>{
+        this.sleepyID = setInterval(()=>{
             const sleepyElement =  document.querySelector('#sleepiness')
             sleepyElement.innerText=`${this.sleepiness}`
-            this.sleepiness ++
+            this.sleepiness++
                 if (this.sleepiness>10) {
-                    clearInterval(sleepyID)
+                    this.endGame()
                 }
         }, 2000)
     }
 
     increaseBoredom() {
-        const boredomID = setInterval(()=>{
+        this.boredomID = setInterval(()=>{
             const boredomElement =  document.querySelector('#boredom')
             boredomElement.innerText=`${this.boredom}`
-            this.boredom ++
+            this.boredom++
                 if (this.boredom>10) {
-                    clearInterval(boredomID)
+                    this.endGame()
                 }
         }, 1500)
     }
 
     morphAge() {
- //     const ageUP = document.querySelector('#kitty')
-  //    let currentWidth = ("#kitty").style.width
-  //    let currentHeight = ("#kitty").style.height
-  //      let newWidth = currentWidth + 50
-   //     let newHeight = currentHeight + 50
-    
+        const ageKitty = document.querySelector("#kitty")
+        this.width+=100
+        this.height+=100
+        ageKitty.style.height=`${this.height}px`
+        ageKitty.style.width=`${this.width}px`
     }
     gameStart() {
         this.increaseHunger()
         this.increaseSleepiness()
         this.increaseBoredom()
         this.increaseAge()
-        this.endGame()
 
     }
     endGame() {
-        if((this.age===10)||(this.sleepiness===10)||(this.boredom===10)) {
-            document.querySelector('.speech').innerText=`Oh no! I'm dead. :(`   
-        }
+        clearInterval(this.boredomID)
+        clearInterval(this.sleepyID)
+        clearInterval(this.hungerID)
+        clearInterval(this.ageID)
+        const deadKitty = document.querySelector('.speech')
+        deadKitty.innerText=`Oh no! I'm dead.`
+        const deadKitty2 = document.querySelector('#kitty')
+        deadKitty2.setAttribute('src','Images/Sad.png')   
+ 
     }
 }
 
 
 const Tamagotchi1 = new Tamagotchi()
 
-document.querySelector("#feed-Me").addEventListener('click', Tamagotchi1.feedPet)
-document.querySelector('#pet-Me').addEventListener('click', Tamagotchi1.giveLove)
-document.querySelector('#put-Me-To-Bed').addEventListener('click', Tamagotchi1.putToBed)
+document.querySelector("#feed-Me").addEventListener('click', ()=>{Tamagotchi1.feedPet()})
+document.querySelector('#pet-Me').addEventListener('click', ()=>{Tamagotchi1.giveLove()})
+document.querySelector('#put-Me-To-Bed').addEventListener('click', ()=>{Tamagotchi1.putToBed()})
 //document.querySelector('#turn-Off-Lights').addEventListener('click', Tamagotchi1.lightSwitchOff)
 //document.querySelector('#turn-Off-Lights').addEventListener('click', Tamagotchi1.lightSwitchOn)
 document.querySelector('#make-Me-Older').addEventListener('click', Tamagotchi1.increaseAge)
-document.querySelector('#play-With-Me').addEventListener('click', Tamagotchi1.playWithPet)
-document.querySelector('#make-Me-Older').addEventListener('click',Tamagotchi1.morphAge)
-document.querySelector('#submit').addEventListener('click', Tamagotchi1.gameStart())
+document.querySelector('#play-With-Me').addEventListener('click', ()=>{Tamagotchi1.playWithPet()})
+document.querySelector('#give-Love').addEventListener('click', ()=>{Tamagotchi1.giveLove()})
+document.querySelector('#make-Me-Older').addEventListener('click', ()=>{Tamagotchi1.morphAge()})
+document.querySelector('#submit').addEventListener('click', ()=>{Tamagotchi1.gameStart()})
 
-console.log(Tamagotchi1.feedPet)
